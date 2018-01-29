a23c7c269ee7f95e0aab1f796e6c693def3b977a
shivam <shvmsngh91@gmail.com>
2018-01-29 14:47:17 +0530

    refactor viewmodel module to contain all viewmodel provider factory with qualifier.

---
+++ app/src/main/java/com/tracker/ui/TrackerViewModelModule.java
@@ -0,0 +1,53 @@
+package com.tracker.ui;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.tracker.di.qualifiers.ForApplication;
+import com.tracker.ui.trackdetail.TrackDetailViewModel;
+import com.tracker.ui.tracker.LocationProvider;
+import com.tracker.ui.tracker.ReactiveGpsLocationProvider;
+import com.tracker.ui.tracker.TrackerViewModel;
+import com.tracker.ui.tracks.TracksViewModel;
+import com.tracker.ui.util.ViewModelUtil;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class TrackerViewModelModule {
+
+    @Singleton
+    @Provides
+    @Named("TrackerViewModel")
+    static ViewModelProvider.Factory trackerViewModelProviderFactory(ViewModelUtil viewModelUtil,
+                                                              TrackerViewModel trackerViewModel) {
+        return viewModelUtil.createFor(trackerViewModel);
+    }
+
+    @Singleton
+    @Provides
+    @Named("TracksViewModel")
+    static ViewModelProvider.Factory tracksViewModelProviderFactory(ViewModelUtil viewModelUtil,
+                                                              TracksViewModel trackerViewModel) {
+        return viewModelUtil.createFor(trackerViewModel);
+    }
+
+    @Singleton
+    @Provides
+    @Named("TrackDetailViewModel")
+    static ViewModelProvider.Factory trackDetailViewModelProviderFactory(ViewModelUtil viewModelUtil,
+                                                              TrackDetailViewModel trackerViewModel) {
+        return viewModelUtil.createFor(trackerViewModel);
+    }
+
+    @Singleton
+    @Provides
+    LocationProvider locationProvider(@NonNull @ForApplication Context context) {
+        return new ReactiveGpsLocationProvider(context);
+    }
+}
